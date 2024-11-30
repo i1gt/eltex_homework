d6768148ad0e7ec4ba647cf01f54b4ad9c7df15e
bond <faqbond@gmail.com>
2024-11-30 09:09:15 +0700

    m4task1 done

--- moduel_3/task1/task1.c
+++ moduel_3/task1/task1.c
@@ -8,11 +8,13 @@
 #include <signal.h>
 #include <string.h>
 #define BUFSIZE 4096
-void termh ()
+
+void abrt_handler()
 {
-printf ("Process terminated making fifo\n");
 const char *fifo = "fifo";
-    
+char buf[BUFSIZE];
+
+printf ("Process terminated making fifo\n");
     if (mkfifo(fifo, 0777) == -1)
     {
         unlink(fifo);
@@ -25,46 +27,55 @@ const char *fifo = "fifo";
             exit (EXIT_FAILURE);
         }
     };
+
 char msg[] = "process was terminated by signal";
 int fd = open(fifo, O_RDWR);
 int fdw = write (fd, &msg, strlen(msg));
-char buf[BUFSIZE];
 int fdr = read(fd, &buf, sizeof(buf));
+printf("\n%s \n", buf);
+
 if (fd == -1 || fdw == -1 || fdr == -1) 
     {
         printf("Oops something went wrong");
         exit (EXIT_FAILURE);
-    }
-
+    } 
 
 exit (EXIT_SUCCESS);
-
 }
 
 int main (int argc, char ** argv)
 {
     struct sigaction sa;
-    sa.sa_handler = termh;
-    sigaction(SIGTERM, &sa, 0);
+    sa.sa_handler = abrt_handler;
+    sigaction(SIGABRT, &sa, 0);
 
-    printf("PID = %i\nType 1 to SIGTERM the process\nType 2 to use any siganl\nType 3 to exit\n", getpid());
-    for(int t = 0; t!=-1;)
+    printf("PID = %i\n"
+            "Type 1 to SIGABRT the process\n"
+            "Type 2 to use any siganl\n"
+            "Type 3 to exit\n", getpid());
+
+    int t = 0;
+    while (1)
     {
-        scanf("%d", &t);
-        if (t==1)
-            {
-            kill (getpid(), 15);
-            }
-        if (t==2)
-            {
-                int z;
-                printf("Choose signal\n");
-                scanf("%d", &z);
-                kill (getpid(), z);
-            }
-        if (t==3)
+    scanf("%d", &t);
+    switch (t)
         {
-            return 1;
+        case 1: 
+            kill (getpid(), SIGABRT);
+            break;
+
+        case 2:
+            int z;
+            printf("Choose signal\n");
+            scanf("%d", &z);
+            kill (getpid(), z);
+            break;
+
+        case 3:
+            exit (EXIT_SUCCESS);
+
+        default:
+            printf("Invalid option");
         }
     }
     return EXIT_FAILURE;
